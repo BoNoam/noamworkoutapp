580c760e5dc6fb0f5c3cfe7bd41982ad7dbb0803
Noam Borenstein <noamborenstein@Noams-MacBook-Pro.local>
2026-03-30 23:03:40 -0400

    add service worker

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE = 'lift-v1';
+const ASSETS = ['/noamworkoutapp/', '/noamworkoutapp/index.html'];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE).then(function(cache) {
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(keys) {
+      return Promise.all(
+        keys.filter(function(k) { return k !== CACHE; })
+            .map(function(k) { return caches.delete(k); })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    fetch(e.request).then(function(response) {
+      const clone = response.clone();
+      caches.open(CACHE).then(function(cache) {
+        cache.put(e.request, clone);
+      });
+      return response;
+    }).catch(function() {
+      return caches.match(e.request);
+    })
+  );
+});
